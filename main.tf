58b760f59a02be5535dc402e118402994dea81ab
Ben Potter <me@bpmct.net>
2025-08-24 07:35:08 -0700

    fix: jupyterlab version

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "coder_external_auth" "gcp" {
 module "jupyterlab" {
   count   = data.coder_workspace.me.start_count
   source  = "registry.coder.com/coder/jupyterlab/coder"
-  version = "1.2.1"
+  version = "1.2.0"
   agent_id = coder_agent.main.id
   config = {
     ServerApp = {
